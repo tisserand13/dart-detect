fa0602c7084e09348b9e38946713767cf0746c59
Martin Thissen <thissen.martin@gmail.com>
2018-03-27 19:58:31 +0200

    Tiny configuration changes

--- init.py
+++ init.py
@@ -1,7 +1,8 @@
 import numpy as np
 import cv2
 cap = cv2.VideoCapture(0)
-fgbg = cv2.createBackgroundSubtractorMOG2(60,25,False)
+fgbg = cv2.createBackgroundSubtractorMOG2(60,36,False)
+#By coloring the static background with a high contrast color to the dart, the background subtraction can be improved
 while(1):
     ret, frame = cap.read()
     fgmask = fgbg.apply(frame)
